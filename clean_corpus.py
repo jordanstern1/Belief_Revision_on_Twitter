ba70ff41fa571307f82eda67587e30c500add23a
Jordan Stern <jordan.e.stern@gmail.com>
2018-03-28 17:43:19 -0600

    get_most_common_words() is working

--- clean_corpus.py
+++ clean_corpus.py
@@ -1,52 +1,22 @@
-from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from nltk.stem.porter import *
 import sklearn.decomposition
 import string
 from nltk import word_tokenize
 from nltk.corpus import stopwords
 import pdb
-
+import numpy as np
 from nltk.tokenize import TweetTokenizer
-
 from get_tweets import unpickle_results
+from collections import Counter
+
 
 def load_tweet_corpus(fname):
     tweets = unpickle_results(fname)
     tweet_text = [t.all_text for t in tweets]
-    tweet_corpus = list(set(tweet_text)) # remove duplicates
+    tweet_corpus = np.array(list((set(tweet_text)))) # remove duplicates
 
     return tweet_corpus
-
-
-def clean_tokenize_stem(corpus, stem=False):
-    '''
-    input: np array where each row is a string containing all text from each doc
-    in the corpusd
-    output: same as input, but all characters have been lowercased, stopwords
-    have been removed, and words have been stemmed
-    '''
-    cleaned_corpus = []
-    for doc in corpus:
-        # lowercase and remove punc
-        lowered_str_no_punc = " ".join([word.strip(punctuation)
-                              for word in doc.lower().split()])
-
-        # tokenize
-        tokenized = word_tokenize(lowered_str_no_punc)
-
-        # remove stopwords
-        additional_stopwords = []
-        stopwordz = set(stopwords.words('english') + additional_stopwords)
-        tokenized_no_stopwords = [word for word in tokenized if word not in stopwordz]
-
-        if stem:
-            stemmer = PorterStemmer()
-            result = " ".join([stemmer.stem(word) for word in tokenized_no_stopwords])
-            cleaned_corpus.append(result)
-        else:
-            cleaned_corpus.append(tokenized_no_stopwords)
-
-    return cleaned_corpus
 
 
 def clean_tokenize(tweet_corpus):
@@ -62,23 +32,57 @@ def clean_tokenize(tweet_corpus):
         tokenized = tweet_tokenizer(tweet)
         cleaned_tokenized_corpus.append(tokenized)
 
-
     return cleaned_tokenized_corpus
 
-def tweet_tokenizer(tweet_text):
+
+def tweet_tokenizer(tweet_text, stem=True):
     tknzr = TweetTokenizer(preserve_case=False, strip_handles=True,
                            reduce_len=True)
-    #from string import punctuation
-    punctuation = string.cpunctuation.replace('#','')
+
+    punctuation = string.punctuation.replace('#','')
     tokenized = tknzr.tokenize(tweet_text)
     tokenized = list(map(lambda x: x.strip(punctuation).replace("'",''),
                          tokenized))
     tokenized = list(filter(lambda x: x not in punctuation and len(x) > 1
                             and 'http' not in x, tokenized))
 
+    if stem:
+        stemmer = PorterStemmer()
+        tokenized = [stemmer.stem(word) for word in tokenized]
+
     return tokenized
 
+
+def get_tf_idf(raw_tweet_corpus):
+    vec = TfidfVectorizer(strip_accents='ascii',tokenizer=tweet_tokenizer,
+                          stop_words='english', max_features=None)
+    corpus_tf_idf = vec.fit_transform(raw_tweet_corpus)
+
+    return vec, corpus_tf_idf
+
+
+def get_tf_matrix(raw_tweet_corpus):
+    """ get bag of words """
+    cv = CountVectorizer(strip_accents='ascii',tokenizer=tweet_tokenizer,
+                          stop_words='english', max_features=None)
+    corpus_tf_mat = cv.fit_transform(raw_tweet_corpus)
+
+    return cv, corpus_tf_mat
+
+
+def get_most_common_words(raw_tweet_corpus):
+    cv, corpus_tf_mat = get_tf_matrix(raw_tweet_corpus)
+    words = cv.get_feature_names()
+    term_frequencies = corpus_tf_mat.sum(axis=0).tolist()[0]
+
+    result = Counter(dict(zip(words,term_frequencies))).most_common()
+
+    return result
+
 if __name__ == '__main__':
-    tweet_corpus = load_tweet_corpus('data/query_results_03_26_2018_1.pkl')
-    cleaned_tokenized_corpus = clean_tokenize(tweet_corpus)
-    #cleaned_tweet = tweet_tokenizer(tweet_corpus[101])
+    raw_tweet_corpus = load_tweet_corpus('data/query_results_03_26_2018_1.pkl')
+
+    most_common = get_most_common_words(raw_tweet_corpus)
+
+    # vocab = Counter(vec.vocabulary_)
+    # most_common_words = vocab.most_common()
